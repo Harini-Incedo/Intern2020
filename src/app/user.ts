0fa0f29033aea16cab51960374b86ee99a7c4caf
aman-swaroop <aman.swaroop@incedoinc.com>
2020-06-23 15:11:07 -0700

    Made list page, started create page

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  department: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  timeZone: string;
+  workingHours: string;
+  managerName: string;
+}
