f075265af6f5fd48482ec3676b22f0e439f4aecd
musswa <musswauw@uw.edu>
2020-06-24 15:09:15 -0700

    postgres configuration set up

--- backend/app/src/main/java/com/example/demo/controllers/EmployeeController.java
+++ backend/app/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.Employee;
-import com.example.demo.EmployeeRepository;
+import com.example.demo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
