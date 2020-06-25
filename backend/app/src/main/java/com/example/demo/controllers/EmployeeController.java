6df16a523ec7d598bc3a76d32515aa0c068e7966
musswa <musswauw@uw.edu>
2020-06-25 12:32:10 -0700

    get employee detail, create employee, and delete employee APIs implemented

--- backend/app/src/main/java/com/example/demo/controllers/EmployeeController.java
+++ backend/app/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -3,12 +3,10 @@ package com.example.demo.controllers;
 import com.example.demo.entities.Employee;
 import com.example.demo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -16,14 +14,41 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository repository;
 
+
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
-        return (List<Employee>) repository.findAll();
+        return repository.findAllActive();
     }
 
-//    @PostMapping("/employees")
-//    void addUser(@RequestBody Employee e) {
-//        repository.save(e);
-//    }
+
+    @GetMapping("employees/{id}")
+    public Employee getEmployeeByID(@PathVariable("id") Long id) {
+        Optional<Employee> employee = repository.findById(id);
+        if (employee.isPresent()) {
+            Employee toReturn = employee.get();
+            // if there exists an active employee with the given ID
+            if (toReturn.isActive()) {
+                return toReturn;
+            }
+        }
+        return null;
+    }
+
+
+    @PostMapping("/employees")
+    void createEmployee(@RequestBody Employee e) {
+        e.setActive(true);
+        repository.save(e);
+    }
+
+
+    @DeleteMapping("employees/{id}")
+    void deleteEmployeeByID(@PathVariable("id") Long id) {
+        Employee toDelete = getEmployeeByID(id);
+        if (toDelete != null) {
+            toDelete.setActive(false);
+            repository.save(toDelete);
+        }
+    }
 
 }
