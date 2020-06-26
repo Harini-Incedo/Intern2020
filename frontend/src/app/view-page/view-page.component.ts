979ae96dbd7b9343de05b35e29ceb319f7242e66
aman-swaroop <aman.swaroop@incedoinc.com>
2020-06-26 10:36:51 -0700

    Fixed spacing between buttons | Added reload to delete button

--- frontend/src/app/view-page/view-page.component.ts
+++ frontend/src/app/view-page/view-page.component.ts
@@ -29,11 +29,9 @@ export class ViewPageComponent implements OnInit {
     let check = confirm("Are you sure you want to delete this employee?");
     if (check) {
       this.userService.delete(+user.id).subscribe(d=>this.userService.gotoUserList());
+      window.location.replace("/employees");
+      window.location.reload();
     }
-  }
-
-  reload(): void{
-    window.location.reload();
   }
 
 }
